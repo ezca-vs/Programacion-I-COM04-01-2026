38c46d463eedfdd60ddabb45d423ae50c5f1b6ab
Ezequiel David Cañete <edcanete@campus.ungs.edu.ar>
2026-03-10 20:15:22 -0300

    feat: add exercises from class

---
+++ src/introduccionajava/InputDelUsuario.java
@@ -0,0 +1,10 @@
+package introduccionajava;
+
+public class InputDelUsuario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/introduccionajava/MetodosEnClase.java
@@ -0,0 +1,10 @@
+package introduccionajava;
+
+public class MetodosEnClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
